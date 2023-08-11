965868d38fd2872683a524a1de96040b3a920ee1
Flip Tip <swagger_aged0y@icloud.com>
2023-08-11 19:14:24 +0000

    Hello World

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,20 @@
-<h1>Hello world</h1>
+<div>
+	<p>Hello World</p>
+</div>
+
+<style>
+	:global(body) {
+		background-color: theme(colors.black);
+		color: theme(colors.white);
+	}
+	div {
+		border: grey solid 1px;
+		border-radius: theme(borderRadius.xl);
+		margin: theme(margin.3);
+		color: white;
+	}
+	p, a, h1, h2, h3, h4, h5, h6, span {
+		padding: theme(padding.2);
+		color: white;
+	}
+</style>
